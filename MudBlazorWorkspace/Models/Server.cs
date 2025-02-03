6e7430991a689680bf776d8fd5f9cdfc51ea97ee
MarlonnLima <goncalvesmarlon072@gmail.com>
2025-02-03 19:55:56 -0300

    Adicionar arquivos de projeto.

---
+++ MudBlazorWorkspace/Models/Server.cs
@@ -0,0 +1,10 @@
+﻿namespace MudBlazorWorkspace.Models
+{
+    public class Server
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string IpAddress { get; set; }
+    }
+}
